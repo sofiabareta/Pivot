1a51ac5df84434d2853c2d4e28cbfbcd43782872
sofiabareta <sofibareta@gmail.com>
2014-11-05 13:58:39 -0200

    teste pivot

---
+++ script/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+ 
+  $("#carrossel").owlCarousel({
+ 
+      autoPlay: 3000, //Set AutoPlay to 3 seconds
+ 
+      items : 1,
+      itemsDesktop : [1199,3],
+      itemsDesktopSmall : [979,3]
+ 
+  });
+ 
+});
